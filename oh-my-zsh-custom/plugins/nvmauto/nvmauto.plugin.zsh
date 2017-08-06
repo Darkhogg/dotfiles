f53bda8e05896333e33771de76a11425068f8de2
Daniel Escoz <darkhogg@gmail.com>
2017-08-06 12:01:28 +0200

    Update nvmauto plugin

--- oh-my-zsh-custom/plugins/nvmauto/nvmauto.plugin.zsh
+++ oh-my-zsh-custom/plugins/nvmauto/nvmauto.plugin.zsh
@@ -5,6 +5,7 @@ function _nvma_checkparent {
   local searchdir="$(readlink -f .)"
   while [ "$searchdir" != '/' ]; do
     if [ -f "$searchdir/$1" ]; then
+      echo "$searchdir/$1"
       return 0
     fi
 
@@ -15,14 +16,17 @@ function _nvma_checkparent {
 }
 
 function _nvma_nvmuse_cwd {
-  if _nvma_checkparent package.json; then
-    nvm use &>/dev/null
+  # nvm use .nvmrc if present; nvm
+  local nvmrc="$(_nvma_checkparent .nvmrc)"
+  if [[ -n $nvmrc ]]; then
+    nvm use --silent "$(cat "$nvmrc")"
+  else
+    nvm use --silent system
+  fi
 
+  if [[ -n "$nvmrc" ]] || _nvma_checkparent package.json >/dev/null; then
     NVMA_PROMPT=yes
   else
-    if [[ -n $NVMA_PROMPT ]]; then
-      nvm use system &>/dev/null
-    fi
     NVMA_PROMPT=
   fi
 }
